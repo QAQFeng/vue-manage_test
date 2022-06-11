7bdc90da5b0b71429ae7fa75961267110049c842
QAQFeng <1725678003@qq.com>
2022-06-11 21:49:51 +0800

    重复点击导航报错处理

--- router/index.js
+++ router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
+
 // 路由是动态添加的，动态路由信息在 permission.js 文件里，children 里的路由信息可以注释掉
 const routes = [
     {
         path: '/',
         name: 'Main',
         component: () => import('../views/Main.vue'),
+        // redirect: '/home',
         children: [
             // {
             //     path: '/home',
@@ -43,10 +45,26 @@ const routes = [
     }
 ]
 
+// Vue.use(Router)
+// const router = new Router({
+//   routes
+// })
+ 
+// const VueRouterPush = Router.prototype.push
+// Router.prototype.push = function push (to) {
+//   return VueRouterPush.call(this, to).catch(err => err)
+// }
+
 const router = new VueRouter({
     mode: 'history',
     routes
 })
+
+// 重复点击导航报错处理
+const VueRouterPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(to) {
+    return VueRouterPush.call(this, to).catch(err => err)
+}
 
 // 对 router 进行全局暴露，这样就不需要每个文件都导入一次 router
 export default router
